cfc8f01e47a50848a717ee7f0c7bfe5101cd91d4
Lillian Szugyi <lillian.szugyi@gmail.com>
2016-01-24 22:44:34 -0800

    create function to populate data to document.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,49 @@
-var openHours = ["10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
+var storeHours = ["10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
+var totalCookies = 0;
 
 function random(maxCustomer, minCustomer) {
   return Math.random() * (maxCustomer - minCustomer) + minCustomer;
 }
 
 function cookiesPerHour(maxCustomer, minCustomer, avgCookies) {
-  return random(maxCustomer, minCustomer) * avgCookies;
+  return Math.floor(random(maxCustomer, minCustomer) * avgCookies);
 }
 
-function Store (maxCustomer, minCustomer, avgCookies) {
+function Store (name, maxCustomer, minCustomer, avgCookies) {
+  this.name = name;
   this.maxCustomer = maxCustomer;
   this.minCustomer = minCustomer;
   this.avgCookies = avgCookies;
   this.cookiesArr = [];
 
-  for (var i = 0; i < openHours.length; i++) {
+  for (var i = 0; i < storeHours.length; i++) {
     var hourlyCookies = cookiesPerHour(this.maxCustomer, this.minCustomer, this.avgCookies);
     this.cookiesArr.push(hourlyCookies);
-  };
+  }
 }
 
-var pikePlace = new Store (17, 88, 5.2);
-var seatacAirport = new Store (6, 24, 1.2);
-var southcenter = new Store (11, 38, 1.9);
-var bellevueSquare = new Store (20, 48, 3.3);
-var alki = new Store (3, 24, 2.6);
+var pikePlace = new Store ("Pike Place Market", 17, 88, 5.2);
+var seatacAirport = new Store ("Seatac Airport", 6, 24, 1.2);
+var southcenter = new Store ("Southcenter", 11, 38, 1.9);
+var bellevueSquare = new Store ("Bellevue Square", 20, 48, 3.3);
+var alki = new Store ("Alki", 3, 24, 2.6);
 
-// Display the values of each array as unordered lists in the browser.
-pikePlace.cookiesArr
+function writeSalesProjection(store, elementId) {
+  var content = "<h3>" + store.name + "</h3><ul>";
 
+  for (var i = 0; i < storeHours.length; i++) {
+    content += "<li>" + storeHours[i] + ": " + store.cookiesArr[i] + "</li>";
+    totalCookies = totalCookies + store.cookiesArr[i]
+  }
 
-// Display the lists on index.html. We will be adding features to this application and working with its layout throughout the week.
+  content += "<p class =\"total\">Total: " + totalCookies + " Cookies Needed Per Day</p></ul><hr>";
+
+  var elStore = document.getElementById(elementId);
+  elStore.innerHTML = content;
+}
+
+writeSalesProjection(pikePlace, "pikePlace");
+writeSalesProjection(seatacAirport, "seatacAirport");
+writeSalesProjection(southcenter, "southcenter");
+writeSalesProjection(bellevueSquare, "bellevueSquare");
+writeSalesProjection(alki, "alki");
